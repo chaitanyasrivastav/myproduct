a89ce6cd8bce3cb9bed7e712b63e736dcb3d8667
Chaitanya Srivastav <chaitanya@Chaitanyas-MacBook-Pro.local>
2022-08-28 11:55:07 +0530

    drf functional view

--- users/views.py
+++ users/views.py
@@ -1,24 +1,24 @@
 from http.client import CREATED
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-import json
-
-from myproduct.custom_exceptions import MethodNotAllowedError, AlreadyExistsError
+from django.http import JsonResponse
+from rest_framework.parsers import JSONParser
+from myproduct.custom_exceptions import AlreadyExistsError
+from .serializers import UserSerializer
+from rest_framework.decorators import api_view
 
 # Create your views here.
-
+@api_view(["POST"])
 def create_user(request):
-    headers = {"Content-Type": "application/json"}
     response_data = {}
-    if request.method == "POST":
-        request_data = json.loads(request.body)
-        user = User.objects.get(username=request_data["firstName"])
-        if user:
-            raise AlreadyExistsError(f"{user.username} already exists. Try a different username.")
-        user = User.objects.create_user(request_data["firstName"], request_data["email"], request_data["password"])
-        user.last_name = request_data["lastName"]
-        user.save()
-        response_data["message"] = f"User {request_data['email']} successfully created."
-        return HttpResponse(json.dumps(response_data), headers=headers, status=CREATED)
-    else:
-        raise MethodNotAllowedError(f"{request.method} method is not allowed for this endpoint.")
+    request_data = JSONParser().parse(request)
+    username = request_data["firstName"]
+    email = request_data["email"]
+    password = request_data["password"]
+    user = User.objects.filter(username=username)
+    if user:
+        raise AlreadyExistsError(f"{username} already exists. Try a different username.")
+    user = User.objects.create_user(username, email, password)
+    user.save()
+    response_data["message"] = f"User {email} successfully created."
+    serializer = UserSerializer(user)
+    return JsonResponse(serializer.data, status=CREATED)
